d5f22a992f10fa17dcf46c2083b5caa58c2cedf0
Malik-Rizwan125 <99021298+Malik-Rizwan125@users.noreply.github.com>
2022-10-10 22:58:37 +0500

    8 question is done

---
+++ Array.js
@@ -0,0 +1,39 @@
+// 1. Write a ts program to read and print elements of array
+// var arr1:number[] = [2,4,5,6,7,9]
+// console.log(arr1[0])
+// console.log(arr1[1])
+// console.log(arr1[2])
+// console.log(arr1[3])
+// console.log(arr1[4])
+// console.log(arr1[5])
+// 2. Write a ts program to print all negative elements in an array.
+//  var arr1:number[] = [-2,-4,-6, -8,-10]
+//  console.log(arr1[0])
+//  console.log(arr1[1])
+//  console.log(arr1[2])
+//  console.log(arr1[3])
+//  console.log(arr1[4])
+// 3. Write a ts program to find sum of all array elements..............
+// var arr1: number[] = [2, 4, 5, 6, 7, 9]
+// // console.log(arr1)
+// var sum = 0
+// var i: number;
+// for (i = 2; i <= arr1.length; i++) {
+//     sum += i;
+//     console.log("sum of all the num :", sum)
+// }
+// 4. Write a ts program to find maximum and minimum element in an array
+// 6. Write a ts program to count total number of even and odd elements in an array.
+// var arr1:number[] =  [2,3,5,6,7,8,9,11,34,22,12,33]
+//  var arr2:number = Number(arr1)
+// for(var i=0 ; i<= arr1.length; i++){
+//     if(i % 2 == 0){
+//         console.log("this is even number" , i )
+//     }else{
+// console.log("This is off number", i)
+//     }
+// }
+// 8. Write a ts program to copy all elements from an array to another array.
+// var arr1:number [] = [2,3,4,5,6]
+// var arr2:number[] = arr1;
+// console.log(arr2)
